4e324ed188ca48380d0f03b3e3cf182d94e8c224
Emma Walker <emma.walker@jesus.ox.ac.uk>
2025-09-03 21:08:08 +0100

    update to clustergcn architecture

--- analysis/evaluation/evaluate_graph_model.py
+++ analysis/evaluation/evaluate_graph_model.py
@@ -1,141 +1,194 @@
-from pathlib import Path
-from typing import List
+from typing import Optional, List
 import time
 
 import typer
-import torch
-from torch_geometric.transforms import ToUndirected
-from torch_geometric.utils import add_self_loops
-from torch_geometric.loader import NeighborSampler
 import numpy as np
 
-from happy.utils.utils import get_device, get_project_dir
+import happy.db.eval_runs_interface as db
 from happy.organs import get_organ
-from happy.utils.utils import set_seed
-from happy.graph.create_graph import (
-    get_raw_data,
-    setup_graph,
-    process_knts,
-    get_groundtruth_patch,
-)
-from happy.graph.graph_supervised import (
-    inference,
-    setup_node_splits,
-    evaluate,
-    evaluation_plots,
-    MethodArg,
-)
+from happy.utils.utils import get_device, get_project_dir, set_seed
+from happy.graph.embeddings_umap import fit_umap, plot_cell_graph_umap, plot_tissue_umap
+from happy.graph.graph_creation.get_and_process import get_and_process_raw_data
+from happy.graph.graph_creation.create_graph import setup_graph
+from happy.graph.enums import FeatureArg, MethodArg, SupervisedModelsArg
+from happy.graph.utils.utils import get_model_eval_path
+from happy.graph.utils.visualise_points import visualize_points
+from happy.graph.utils.evaluation import evaluate, evaluation_plots
+from happy.graph.runners.eval_runner import EvalParams, EvalRunner
+from happy.graph.graph_creation.node_dataset_splits import setup_node_splits
 
 
 def main(
     seed: int = 0,
     project_name: str = typer.Option(...),
     organ_name: str = typer.Option(...),
-    pre_trained_path: str = typer.Option(...),
+    exp_name: str = typer.Option(...),
+    model_weights_dir: str = typer.Option(...),
+    model_name: str = typer.Option(...),
     run_id: int = typer.Option(...),
-    annot_tsv: str = typer.Option(...),
-    patch_files: List[str] = typer.Option([]),
+    x_min: int = 0,
+    y_min: int = 0,
+    width: int = -1,
+    height: int = -1,
+    val_patch_files: Optional[List[str]] = None,
     k: int = 5,
+    feature: FeatureArg = FeatureArg.embeddings,
     group_knts: bool = True,
-    graph_method: MethodArg = MethodArg.intersection,
+    random_remove: float = 0.0,
+    top_conf: bool = False,
+    standardise: bool = True,
+    model_type: SupervisedModelsArg = SupervisedModelsArg.sup_clustergcn,
+    graph_method: MethodArg = MethodArg.k,
+    plot_umap: bool = True,
+    remove_unlabelled: bool = True,
+    tissue_label_tsv: Optional[str] = None,
+    compress_labels: bool = False,
     verbose: bool = True,
-    plot: bool = False,
 ):
-    """Evaluates model performance across validation or test datasets
+    """ Runs inference over a WSI or a region of a WSI. If ground truth data is
+    provided, it will produce evaluation metrics and plots.
 
-    seed: random seed to fix
-    project_name: name of directory containing the project
-    organ_name: name of organ
-    pre_trained_path: path relative to project to pretrained model
-    run_id: evalrun id of embeddings to evaluate over
-    annot_tsv: the name of the annotations file containing ground truth points
-    patch_files: the name of the file(s) containing validation or test patches
-    k: the value of k to use for the kNN or intersection graph
-    group_knts: whether to process KNT predictions
-    graph_method: method for constructing the graph (k, delaunay, intersection)
-    verbose: whether to print to console graph construction progress
-    plot: whether to generate evaluation plots
+    Args:
+        seed: set the random seed for reproducibility
+        project_name: name of the project dir to save results to
+        organ_name: name of organ for getting the cells and tissues
+        exp_name: name of the experiment directory to get the model weights from
+        model_weights_dir: timestamp directory containing model weights
+        model_name: name of the pickle file containing the model weights
+        run_id: run_id cells to evaluate over
+        x_min: bottom left x coordinate of the region to evaluate over (0 for full WSI)
+        y_min: bottom left y coordinate of the region to evaluate over (0 for full WSI)
+        width: width of the region to evaluate over (-1 for full WSI)
+        height: height of the region to evaluate over (-1 for full WSI)
+        val_patch_files: list of files containing validation or test regions
+        k: value of k for kNN graph edge construction method
+        feature: one of 'embeddings' or 'predictions'
+        group_knts: whether to first group knt predictions into one node
+        random_remove: what proportion of the nodes to randomly remove
+        top_conf: whether to filter cells by top confidence predictions only
+        standardise: whether to standardise the cell embedding input features
+        model_type: which type of supervised graph model the weights are from
+        graph_method: which type of edge construction to use
+        plot_umap: whether to plot the UMAP of the cell and tissue embeddings (slow)
+        remove_unlabelled: whether to remove unlabelled nodes from the evaluation
+        tissue_label_tsv: tsv file containing ground truth tissue labels for each cell
+        compress_labels: whether to compress the labels into fewer classes (in organ)
+        verbose: whether to print graph setup
     """
+    db.init()
     set_seed(seed)
     device = get_device()
     project_dir = get_project_dir(project_name)
     organ = get_organ(organ_name)
-    patch_files = [project_dir / "graph_splits" / file for file in patch_files]
+    patch_files = [project_dir / "graph_splits" / file for file in val_patch_files]
 
-    print("Begin graph construction...")
-    predictions, embeddings, coords, confidence = get_raw_data(
-        project_dir, run_id, 0, 0, -1, -1, verbose=verbose
+    # Graph params for saving
+    graph_params = {
+        "run_ids": run_id,
+        "x_min": x_min,
+        "y_min": y_min,
+        "width": width,
+        "height": height,
+        "edge_method": graph_method,
+        "k": k,
+        "feature": feature,
+        "group_knts": group_knts,
+        "random_remove": random_remove,
+        "top_conf": top_conf,
+        "standardise": standardise,
+    }
+    # Get and process raw and ground truth data
+    hdf5_data, tissue_class = get_and_process_raw_data(
+        project_name, organ, project_dir, run_id, graph_params, tissue_label_tsv
     )
-    # Get ground truth manually annotated data
-    _, _, tissue_class = get_groundtruth_patch(
-        organ, project_dir, 0, 0, -1, -1, annot_tsv
-    )
-    # Covert isolated knts into syn and turn groups into a single knt point
-    if group_knts:
-        predictions, embeddings, coords, confidence, tissue_class = process_knts(
-            organ,
-            predictions,
-            embeddings,
-            coords,
-            confidence,
-            tissue_class,
-            verbose=verbose,
-        )
+
     # Covert input cell data into a graph
-    data = setup_graph(coords, k, embeddings, graph_method, loop=False, verbose=verbose)
-    data = ToUndirected()(data)
-    data.edge_index, data.edge_attr = add_self_loops(
-        data["edge_index"], data["edge_attr"], fill_value="mean"
-    )
-    pos = data.pos
-    x = data.x.to(device)
+    data = setup_graph(hdf5_data, organ, feature, k, graph_method, tissue_class)
 
-    data = setup_node_splits(data, tissue_class, True, patch_files, verbose=verbose)
-    print("Graph construction complete")
+    # Split data into validation or test set based on val_patch_files
+    data = setup_node_splits(
+        data, tissue_class, remove_unlabelled, True, patch_files, verbose=verbose
+    )
 
     # Setup trained model
-    pretrained_path = project_dir / pre_trained_path
-    model = torch.load(pretrained_path, map_location=device)
-    model_name = pretrained_path.parts[-1]
-    model_epochs = (
-        "model_final"
-        if model_name == "graph_model.pt"
-        else f"model_{model_name.split('_')[0]}"
+    pretrained_path = (
+        project_dir
+        / "results"
+        / "graph"
+        / model_type.value
+        / exp_name
+        / model_weights_dir
+        / model_name
     )
+    eval_params = EvalParams(data, device, pretrained_path, model_type, 512, organ)
+    eval_runner = EvalRunner.new(eval_params)
 
-    # Setup paths
-    save_path = (
-        Path(*pretrained_path.parts[:-1]) / "eval" / model_epochs / f"run_{run_id}"
-    )
-    save_path.mkdir(parents=True, exist_ok=True)
-
-    # Dataloader for eval, feeds in whole graph
-    eval_loader = NeighborSampler(
-        data.edge_index, node_idx=None, sizes=[-1], batch_size=512, shuffle=False
-    )
-
-    # Run inference and get predicted labels for nodes
     timer_start = time.time()
-    out, graph_embeddings, predicted_labels = inference(model, x, eval_loader, device)
+    # Run inference and get predicted labels for nodes
+    print("Running inference")
+    out, graph_embeddings, predicted_labels = eval_runner.inference()
     timer_end = time.time()
-    print(f"total time: {timer_end - timer_start:.4f} s")
+    print(f"total inference time: {timer_end - timer_start:.4f} s")
+
+    # Setup path to save results
+    save_path = get_model_eval_path(model_name, pretrained_path, run_id)
+    conf_str = "_top_conf" if top_conf else ""
+    plot_name = f"{val_patch_files[0].split('.csv')[0]}{conf_str}"
 
     # restrict to only data in patch_files using val_mask
     val_nodes = data.val_mask
     predicted_labels = predicted_labels[val_nodes]
     out = out[val_nodes]
-    pos = pos[val_nodes]
-    tissue_class = tissue_class[val_nodes]
-
-    # Remove unlabelled (class 0) ground truth points
-    unlabelled_inds, tissue_class, predicted_labels, pos, out = _remove_unlabelled(
-        tissue_class, predicted_labels, pos, out
+    pos = data.pos[val_nodes]
+    graph_embeddings = graph_embeddings[val_nodes]
+    tissue_class = (
+        tissue_class[val_nodes] if tissue_label_tsv is not None else tissue_class
     )
 
+    predictions = hdf5_data.cell_predictions
+    # Remove unlabelled (class 0) ground truth points
+    if remove_unlabelled and tissue_label_tsv is not None:
+        unlabelled_inds, tissue_class, predicted_labels, pos, out = _remove_unlabelled(
+            tissue_class, predicted_labels, pos, out
+        )
+        graph_embeddings = graph_embeddings[unlabelled_inds]
+        predictions = predictions[unlabelled_inds]
+
+    if plot_umap:
+        # fit and plot umap with cell classes
+        fitted_umap = fit_umap(graph_embeddings)
+        plot_cell_graph_umap(
+            organ, predictions, fitted_umap, save_path, f"cell_{plot_name}_umap.png"
+        )
+        # Plot the predicted labels onto the umap of the graph embeddings
+        plot_tissue_umap(organ, fitted_umap, plot_name, save_path, predicted_labels)
+        if tissue_label_tsv is not None:
+            plot_tissue_umap(
+                organ, fitted_umap, f"gt_{plot_name}", save_path, tissue_class
+            )
+
+    # Print some prediction count info
+    tissue_label_mapping = {tissue.id: tissue.label for tissue in organ.tissues}
+    _print_prediction_stats(predicted_labels, tissue_label_mapping)
+
     # Evaluate against ground truth tissue annotations
-    evaluate(tissue_class, predicted_labels, out, organ, True)
-    if plot:
-        evaluation_plots(tissue_class, predicted_labels, organ, save_path)
+    if tissue_label_tsv is not None:
+        _print_prediction_stats(tissue_class, tissue_label_mapping)
+        evaluate(tissue_class, predicted_labels, out, organ, compress_labels)
+        evaluation_plots(tissue_class, predicted_labels, out, organ, save_path)
+
+    # Visualise cluster labels on graph patch
+    print("Generating image")
+    colours_dict = {tissue.id: tissue.colour for tissue in organ.tissues}
+    colours = [colours_dict[label] for label in predicted_labels]
+    visualize_points(
+        organ,
+        save_path / f"{plot_name.split('.png')[0]}.png",
+        pos,
+        colours=colours,
+        width=int(data.pos[:, 0].max()) - int(data.pos[:, 0].min()),
+        height=int(data.pos[:, 1].max()) - int(data.pos[:, 1].min()),
+    )
 
 
 def _remove_unlabelled(tissue_class, predicted_labels, pos, out):
@@ -146,6 +199,15 @@ def _remove_unlabelled(tissue_class, predicted_labels, pos, out):
     out = np.delete(out, 0, axis=1)
     predicted_labels = predicted_labels[labelled_inds]
     return labelled_inds, tissue_class, predicted_labels, pos, out
+
+
+def _print_prediction_stats(predicted_labels, tissue_label_mapping):
+    unique, counts = np.unique(predicted_labels, return_counts=True)
+    unique_labels = []
+    for label in unique:
+        unique_labels.append(tissue_label_mapping[label])
+    unique_counts = dict(zip(unique_labels, counts))
+    print(f"Counts per label: {unique_counts}")
 
 
 if __name__ == "__main__":
--- graph_train.py
+++ graph_train.py
@@ -1,25 +1,20 @@
 from typing import Optional, List
+import json
 
 import typer
-import torch
-from torch_geometric.transforms import ToUndirected
-from torch_geometric.utils import add_self_loops
 from torch_geometric.data import Batch
 
+import happy.db.eval_runs_interface as db
 from happy.utils.utils import get_device
 from happy.organs import get_organ
 from happy.logger.logger import Logger
 from happy.train.utils import setup_run
-from happy.data.setup_dataloader import setup_graph_dataloaders
-from happy.utils.utils import get_project_dir, send_graph_to_device, set_seed
-from happy.graph import graph_supervised
-from happy.graph.graph_supervised import MethodArg
-from happy.graph.create_graph import (
-    get_raw_data,
-    setup_graph,
-    get_groundtruth_patch,
-    process_knts,
-)
+from happy.utils.utils import get_project_dir, set_seed
+from happy.graph.enums import FeatureArg, MethodArg, SupervisedModelsArg
+from happy.graph.graph_creation.get_and_process import get_and_process_raw_data
+from happy.graph.graph_creation.create_graph import setup_graph
+from happy.graph.runners.train_runner import TrainParams, TrainRunner
+from happy.graph.graph_creation.node_dataset_splits import setup_splits_by_runid
 
 
 def main(
@@ -29,67 +24,97 @@ def main(
     exp_name: str = typer.Option(...),
     run_ids: List[int] = typer.Option([]),
     k: int = 5,
+    feature: FeatureArg = FeatureArg.embeddings,
     group_knts: bool = True,
+    random_remove: float = 0.0,
     pretrained: Optional[str] = None,
-    graph_method: MethodArg = MethodArg.intersection,
+    top_conf: bool = False,
+    model_type: SupervisedModelsArg = SupervisedModelsArg.sup_clustergcn,
+    graph_method: MethodArg = MethodArg.k,
     batch_size: int = 200,
     num_neighbours: int = 400,
-    epochs: int = 2000,
-    layers: int = 16,
+    epochs: int = 100,
+    layers: int = typer.Option(...),
     hidden_units: int = 256,
     dropout: float = 0.5,
+    node_dropout: float = 0.0,
     learning_rate: float = 0.001,
+    standardise: bool = True,
     weighted_loss: bool = True,
     use_custom_weights: bool = True,
+    num_workers: int = 12,
     vis: bool = False,
-    annot_tsvs: List[str] = typer.Option([]),
-    val_patch_files: List[str] = typer.Option([]),
-    test_patch_files: Optional[List[str]] = typer.Option([]),
-    validation_step: int = 100,
+    tissue_label_tsvs: List[str] = typer.Option([]),
+    val_patch_files: Optional[List[str]] = [],
+    test_patch_files: Optional[List[str]] = [],
+    include_validation: bool = True,
+    validation_step: int = 50,
 ):
-    """Train a ClusterGCN model by constructing a graph on the saved cell embeddings.
+    """ Train a supervised model on a cell graph for tissue node classification.
 
-    seed: random seed to fix
-    project_name: name of directory containing the project
-    organ_name: name of organ
-    exp_name: a name for this training experiment
-    run_ids: the evalrun ids of the slides to get the embeddings from
-    k: the value of k to use for the kNN or intersection graph
-    group_knts: whether to process KNT predictions
-    pretrained: path to a pretrained model (optional)
-    graph_method: method for constructing the graph (k, delaunay, intersection)
-    batch_size: batch size for training
-    num_neighbours: max number of subgraph size for clustergcn
-    epochs: number of epochs to train for
-    layers: number of graph layers
-    hidden_units: number of hidden units per layer
-    dropout: amount of dropout to apply at each layer
-    learning_rate: the learning rate for the optimizer
-    weighted_loss: whether to use weighted loss
-    use_custom_weights: if using weighted loss, whether to use custom weights
-    vis: whether to use visdom for visualisation
-    annot_tsvs: the name of the annotations file containing ground truth points
-    val_patch_files: the name of the file(s) containing validation patches
-    test_patch_files: the name of the file(s) containing test patches
-    validation_step: the epoch step size for which to perform validation
+    Args:
+        seed: set the random seed for reproducibility
+        project_name: name of the project dir to save results to
+        organ_name: name of organ for getting the cells and tissues
+        exp_name: name of the experiment directory to save results to
+        run_ids: the run_ids of the cell graphs to use for training
+        k: value of k for kNN graph edge construction method
+        feature: one of 'embeddings' or 'predictions'
+        group_knts: whether to first group knt predictions into one node
+        random_remove: what proportion of the nodes to randomly remove
+        pretrained: path to pretraind model (optional)
+        top_conf: whether to filter cells by top confidence predictions only
+        model_type: which type of supervised graph model to use - default clusterGCN
+        graph_method: which type of edge construction to use
+        batch_size: number of nodes per batch
+        num_neighbours: varies by model_type, broadly the sampled neighbours per node
+        epochs: number of epochs to train for
+        layers: number of layers in the model
+        hidden_units: number of hidden units per layer in the model
+        dropout: model parameter dropout rate
+        node_dropout: node dropout rate
+        learning_rate: learning rate for the optimizer
+        standardise: whether to standardise the cell embedding input features
+        weighted_loss: whether to use weighted loss (usually true)
+        use_custom_weights: whether to use custom weights
+        num_workers: number of workers for the dataloader
+        vis: whether to use visdom for visualisation
+        tissue_label_tsvs: list of tsv files containing tissue labels for each run_id
+        val_patch_files: list of files with regions to use for validation
+        test_patch_files: list of files with regions to use for test
+        include_validation: whether to include validation in training
+        validation_step: at how many epochs to run validation
     """
-
+    # general setup
+    db.init()
     device = get_device()
-    graph_method = graph_method.value
     set_seed(seed)
+
+    model_type = model_type.value
+    graph_method = graph_method.value
+    feature = feature.value
 
     project_dir = get_project_dir(project_name)
     pretrained_path = project_dir / pretrained if pretrained else None
     organ = get_organ(organ_name)
 
+    graph_split_files_dir = project_dir / "graph_splits"
     if len(val_patch_files) > 0:
-        val_patch_files = [
-            project_dir / "graph_splits" / file for file in val_patch_files
-        ]
+        val_patch_files = [graph_split_files_dir / file for file in val_patch_files]
     if len(test_patch_files) > 0:
-        test_patch_files = [
-            project_dir / "graph_splits" / file for file in test_patch_files
-        ]
+        test_patch_files = [graph_split_files_dir / file for file in test_patch_files]
+
+    # Graph params for saving
+    graph_params = {
+        "run_ids": run_ids,
+        "edge_method": graph_method,
+        "k": k,
+        "feature": feature,
+        "group_knts": group_knts,
+        "random_remove": random_remove,
+        "top_conf": top_conf,
+        "standardise": standardise,
+    }
 
     # Setup recording of stats per batch and epoch
     logger = Logger(
@@ -98,133 +123,97 @@ def main(
 
     datas = []
     for i, run_id in enumerate(run_ids):
-        # Get training data from hdf5 files
-        predictions, embeddings, coords, confidence = get_raw_data(
-            project_dir, run_id, 0, 0, -1, -1
-        )
-        # Get ground truth manually annotated data
-        _, _, tissue_class = get_groundtruth_patch(
-            organ, project_dir, 0, 0, -1, -1, annot_tsvs[i]
-        )
-        # Covert isolated knts into syn and turn groups into a single knt point
-        if group_knts:
-            predictions, embeddings, coords, confidence, tissue_class = process_knts(
-                organ, predictions, embeddings, coords, confidence, tissue_class
-            )
-        # Covert input cell data into a graph
-        data = setup_graph(coords, k, embeddings, graph_method, loop=False)
-        data.y = torch.Tensor(tissue_class).type(torch.LongTensor)
-        data = ToUndirected()(data)
-        data.edge_index, data.edge_attr = add_self_loops(
-            data["edge_index"], data["edge_attr"], fill_value="mean"
+        # Get and process raw and ground truth data
+        hdf5_data, tissue_class = get_and_process_raw_data(
+            project_name, organ, project_dir, run_id, graph_params, tissue_label_tsvs[i]
         )
 
-        # Split nodes into unlabelled, training and validation sets. So far, validation
-        # and test sets are only defined for run_id 1. If there is training data in
-        # tissue_class for other runs, that data will also be used for training.
-        if run_id == 1:
-            data = graph_supervised.setup_node_splits(
-                data,
-                tissue_class,
-                True,
-                val_patch_files,
-                test_patch_files,
-            )
-        else:
-            data = graph_supervised.setup_node_splits(data, tissue_class, True)
+        # Covert input cell data into a graph
+        data = setup_graph(hdf5_data, organ, feature, k, graph_method, tissue_class)
+
+        # Split data into train, val, test sets based on patch files and run_id
+        data = setup_splits_by_runid(
+            data,
+            run_id,
+            tissue_class,
+            include_validation,
+            val_patch_files,
+            test_patch_files,
+        )
         datas.append(data)
 
     # Combine multiple graphs into a single graph
     data = Batch.from_data_list(datas)
 
-    # Setup the dataloader which minibatches the graph
-    train_loader, val_loader = setup_graph_dataloaders(data, batch_size, num_neighbours)
-
-    # Setup the training parameters
-    x, _, _ = send_graph_to_device(data, device)
-
-    # Setup the model
-    model = graph_supervised.setup_model(
+    # Setup training parameters, including dataloaders, models, loss, etc.
+    run_params = TrainParams(
         data,
         device,
-        layers,
-        len(organ.tissues),
-        hidden_units,
-        dropout,
         pretrained_path,
-    )
-
-    # Setup training parameters
-    optimiser, criterion = graph_supervised.setup_training_params(
-        model,
-        organ,
-        learning_rate,
-        train_loader,
-        device,
-        weighted_loss,
-        use_custom_weights,
-    )
-
-    # Saves each run by its timestamp and record params for the run
-    run_path = setup_run(project_dir, exp_name, "graph")
-    params = graph_supervised.collect_params(
-        seed,
-        organ_name,
-        exp_name,
-        run_ids,
-        0,
-        0,
-        -1,
-        -1,
-        k,
-        graph_method,
+        model_type,
         batch_size,
         num_neighbours,
-        learning_rate,
         epochs,
         layers,
+        hidden_units,
+        dropout,
+        node_dropout,
+        learning_rate,
+        num_workers,
         weighted_loss,
         use_custom_weights,
+        validation_step,
+        organ,
     )
-    params.to_csv(run_path / "params.csv", index=False)
+    train_runner = TrainRunner.new(run_params)
 
-    # Train!
+    # Saves each run by its timestamp and record params for the run
+    run_path = setup_run(project_dir, f"{model_type}/{exp_name}", "graph")
+    _save_graph_params(graph_params, run_path)
+    train_runner.params.save(seed, exp_name, run_path)
+
+    # train!
+    train(train_runner, logger, run_path, include_validation)
+
+
+def train(train_runner, logger, run_path, include_validation):
+    train_runner.prepare_data()
+    epochs = train_runner.params.epochs
+    validation_step = train_runner.params.validation_step
+
     try:
         print("Training:")
         prev_best_val = 0
         for epoch in range(1, epochs + 1):
-            loss, accuracy = graph_supervised.train(
-                model,
-                optimiser,
-                criterion,
-                train_loader,
-                device,
-            )
+            loss, accuracy = train_runner.train()
             logger.log_loss("train", epoch - 1, loss)
             logger.log_accuracy("train", epoch - 1, accuracy)
 
-            if epoch % validation_step == 0 or epoch == 1:
-                train_accuracy, val_accuracy = graph_supervised.validate(
-                    model, data, val_loader, device
-                )
+            if include_validation and (epoch % validation_step == 0 or epoch == 1):
+                train_accuracy, val_accuracy = train_runner.validate()
                 logger.log_accuracy("train_inf", epoch - 1, train_accuracy)
                 logger.log_accuracy("val", epoch - 1, val_accuracy)
-
+                logger.to_csv(run_path / "graph_train_stats.csv")
                 # Save new best model
                 if val_accuracy >= prev_best_val:
-                    graph_supervised.save_state(run_path, logger, model, epoch)
+                    train_runner.save_state(run_path, epoch)
                     print("Saved best model")
                     prev_best_val = val_accuracy
 
     except KeyboardInterrupt:
         save_hp = input("Would you like to save anyway? y/n: ")
         if save_hp == "y":
-            # Save the fully trained model
-            graph_supervised.save_state(run_path, logger, model, epoch)
+            # Save the interrupted model
+            train_runner.save_state(run_path, epoch)
 
-    # Save the fully trained model
-    graph_supervised.save_state(run_path, logger, model, "final")
+    # Save the final trained model
+    train_runner.save_state(run_path, "final")
     print("Saved final model")
+
+
+def _save_graph_params(graph_params_dict, run_path):
+    with open(run_path / "graph_params.json", "w") as f:
+        json.dump(graph_params_dict, f, indent=2)
 
 
 if __name__ == "__main__":
