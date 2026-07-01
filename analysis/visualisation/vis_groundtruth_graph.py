e186371046884634c71ae4655757416e49a9e020
Emma Walker <emma.walker@jesus.ox.ac.uk>
2026-07-01 10:43:33 +0100

    update imports for graph related code

--- analysis/visualisation/vis_groundtruth_graph.py
+++ analysis/visualisation/vis_groundtruth_graph.py
@@ -1,10 +1,10 @@
 import typer
 import numpy as np
 
-from happy.graph.create_graph import get_groundtruth_patch
+from happy.graph.graph_creation.get_and_process import get_groundtruth_patch
 from happy.utils.utils import get_project_dir
 from happy.organs import get_organ
-from happy.graph.visualise import visualize_points
+from happy.graph.utils.visualise_points import visualize_points
 
 
 def main(
